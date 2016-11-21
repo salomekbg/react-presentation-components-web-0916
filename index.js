3d1d4c2392e60f8f8ac92e81a1a9846a4aa567fd
SluvsP <salomekbg@gmail.com>
2016-11-20 19:03:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much.";
+const HelloWorld = React.createClass({
+  render: function() {
+    return hi
+    // <div class="hello-world">Hello {this.props.message || 'World'}</div>;
+  }
+})
+
+// const defaultLimit = 100
+//
+// export default class TextField extends React.Component {
+//   render() {
+//     return (
+//       <input
+//         className="field field-light"
+//         onChange={this.props.onChange}
+//         maxLength={this.props.limit || defaultLimit} />
+//     )
+//   }
+// }
+
+const defaultLimit = 100
+
+export default function TextField(props) {
+  return (
+    hi
+    // <input
+    //   className="field field-light"
+    //   onChange={this.props.onChange}
+    //   limit={this.props.limit || defaultLimit} />
+  )
+}
